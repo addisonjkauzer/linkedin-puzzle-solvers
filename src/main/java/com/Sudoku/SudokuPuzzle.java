3f95c747631b25a63d4b612d4bc0fc311b59fe64
Addison Kauzer <addisonjkauzer@gmail.com>
2026-02-27 20:07:44 -0800

    Fix Sudoku solver loop by resetting isSolved flag each iteration

--- src/main/java/com/Sudoku/SudokuPuzzle.java
+++ src/main/java/com/Sudoku/SudokuPuzzle.java
@@ -1,18 +1,18 @@
 package com.Sudoku;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.function.Consumer;
 
-@AllArgsConstructor
 @Getter
 public class SudokuPuzzle {
 
     private final int[][] board;
+
+    private final HashMap<String, Set<Integer>> possibleValuesGrid = new HashMap<>();
 
     private final static Integer[][] firstQuadrant = {{0,0}, {0,1}, {0,2}, {1,0}, {1,1}, {1,2}};
     private final static Integer[][] secondQuadrant = {{0,3}, {0,4}, {0,5}, {1,3}, {1,4}, {1,5}};
@@ -21,94 +21,151 @@ public class SudokuPuzzle {
     private final static Integer[][] fifthQuadrant = {{4,0}, {4,1}, {4,2}, {5,0}, {5,1}, {5,2}};
     private final static Integer[][] sixthQuadrant = {{4,3}, {4,4}, {4,5}, {5,3}, {5,4}, {5,5}};
 
+
+    public SudokuPuzzle(final int[][] board) {
+        this.board = board;
+        for (int row = 0; row < this.board.length; row++) {
+            for (int col = 0; col < this.board[row].length; col++) {
+                if (this.board[row][col] == 0) {
+                    updatePossibleValues(row, col);
+                } else {
+                    possibleValuesGrid.put(row + "," + col, new HashSet<>(Set.of(this.board[row][col])));
+                }
+            }
+        }
+    }
+
     public void solve() {
         solve(null);
     }
 
     public void solve(Consumer<int[]> onCellVisited) {
         boolean isSolved = true;
-        for (int row = 0; row < board.length; row++) {
-            for (int col = 0; col < board[row].length; col++) {
-                if (board[row][col] != 0) {
-                    if (onCellVisited != null) {
-                        onCellVisited.accept(new int[]{row, col, 0});
+        do {
+            for (int row = 0; row < board.length; row++) {
+                for (int col = 0; col < board[row].length; col++) {
+                    if (board[row][col] != 0) {
+                        if (onCellVisited != null) {
+                            onCellVisited.accept(new int[]{row, col, 0});
+                        }
+                        continue;
                     }
-                    continue;
-                }
-                final Set<Integer> existingValues = new HashSet<>();
-                existingValues.addAll(getAllValuesFromRow(row));
-                existingValues.addAll(getAllValuesFromCol(col));
-                existingValues.addAll(getAllValuesFromBlock(row, col));
+                    final Set<Integer> possibleValues = updatePossibleValues(row, col);
+                    if (possibleValues.size() == 1) {
+                        submitCell(row, col, possibleValues.stream().findFirst().get(), onCellVisited);
+                        continue;
+                    }
 
-                final Set<Integer> possibleValues = new HashSet<>(Set.of(1, 2, 3, 4, 5, 6));
-                possibleValues.removeAll(existingValues);
-                if (possibleValues.size() == 1) {
-                    board[row][col] = possibleValues.stream().findFirst().get();
-                    if (onCellVisited != null) {
-                        onCellVisited.accept(new int[]{row, col, board[row][col]});
+                    final Set<Integer> remainingPossibleInRow = getRemainingPossibleInRow(row, col);
+                    if (remainingPossibleInRow.size() == 1) {
+                        submitCell(row, col, remainingPossibleInRow.stream().findFirst().get(), onCellVisited);
+                        continue;
                     }
-                } else {
+
+                    final Set<Integer> remainingPossibleInCol = getRemainingPossibleInCol(row, col);
+                    if (remainingPossibleInCol.size() == 1) {
+                        submitCell(row, col, remainingPossibleInCol.stream().findFirst().get(), onCellVisited);
+                        continue;
+                    }
+
+                    final Set<Integer> remainingPossibleInBlock = getRemainingPossibleInBlock(row, col);
+                    if (remainingPossibleInBlock.size() == 1) {
+                        submitCell(row, col, remainingPossibleInBlock.stream().findFirst().get(), onCellVisited);
+                        continue;
+                    }
+
                     isSolved = false;
                     if (onCellVisited != null) {
                         onCellVisited.accept(new int[]{row, col, 0});
                     }
                 }
             }
-        }
-        if (!isSolved) {
-            solve(onCellVisited);
-        }
+        } while (!isSolved);
     }
 
-    private Set<Integer> getAllValuesFromCol(int col) {
-        final Set<Integer> values = new HashSet<>();
+    private Set<Integer> updatePossibleValues(int row, int col) {
+
+        final Set<Integer> values = new HashSet<>(Set.of(1, 2, 3, 4, 5, 6));
         for (int i = 0; i < board.length; i++) {
             if (board[i][col] != 0) {
-                values.add(board[i][col]);
+                values.remove(board[i][col]);
             }
         }
-        return values;
-    }
-
-    private Set<Integer> getAllValuesFromRow(int row) {
-        final Set<Integer> values = new HashSet<>();
         for (int i = 0; i < board[row].length; i++) {
             if (board[row][i] != 0) {
-                values.add(board[row][i]);
+                values.remove(board[row][i]);
+            }
+        }
+        final Integer[][] blockCoords = getBlockCoords(row, col);
+        for (Integer[] coord : blockCoords) {
+            if (board[coord[0]][coord[1]] != 0) {
+                values.remove(board[coord[0]][coord[1]]);
+            }
+        }
+        possibleValuesGrid.put(row + "," + col, values);
+        return values;
+    }
+
+    private Set<Integer> getRemainingPossibleInCol(int row, int col) {
+        final Set<Integer> values = new HashSet<>(Set.of(1, 2, 3, 4, 5, 6));
+        for (int i = 0; i < board.length; i++) {
+            if (i != row) {
+                values.removeAll(possibleValuesGrid.get(i + "," + col));
             }
         }
         return values;
     }
 
+    private Set<Integer> getRemainingPossibleInRow(int row, int col) {
+        final Set<Integer> values = new HashSet<>(Set.of(1, 2, 3, 4, 5, 6));
+        for (int i = 0; i < board[row].length; i++) {
+            if (i != col) {
+                values.removeAll(possibleValuesGrid.get(row + "," + i));
+            }
+        }
+        return values;
+    }
 
-    private Set<Integer> getAllValuesFromBlock(int row, int col) {
-        Integer[][] blockCoords;
+    private Set<Integer> getRemainingPossibleInBlock(int row, int col) {
+        final Set<Integer> values = new HashSet<>(Set.of(1, 2, 3, 4, 5, 6));
+        final Integer[][] blockCoords = getBlockCoords(row, col);
+        for (Integer[] coord : blockCoords) {
+            if (coord[0] == row && coord[1] == col) {
+                continue;
+            }
+            values.removeAll(possibleValuesGrid.get(coord[0] + "," + coord[1]));
+        }
+        return values;
+    }
+
+    private void submitCell(int row, int col, int value, Consumer<int[]> onCellVisited) {
+        board[row][col] = value;
+        possibleValuesGrid.put(row + "," + col, Set.of(value));
+        if (onCellVisited != null) {
+            onCellVisited.accept(new int[]{row, col, board[row][col]});
+        }
+    }
+
+    private Integer[][] getBlockCoords(int row, int col) {
         if (row < 2) {
             if (col < 3) {
-                blockCoords = firstQuadrant;
+                return firstQuadrant;
             } else {
-                blockCoords = secondQuadrant;
+                return secondQuadrant;
             }
         } else if (row < 4) {
             if (col < 3) {
-                blockCoords = thirdQuadrant;
+                return thirdQuadrant;
             } else {
-                blockCoords = fourthQuadrant;
+                return fourthQuadrant;
             }
         } else {
             if (col < 3) {
-                blockCoords = fifthQuadrant;
+                return fifthQuadrant;
             } else {
-                blockCoords = sixthQuadrant;
+                return sixthQuadrant;
             }
         }
-        Set<Integer> values = new HashSet<>();
-        for (Integer[] coord : blockCoords) {
-            if (board[coord[0]][coord[1]] != 0) {
-                values.add(board[coord[0]][coord[1]]);
-            }
-        }
-        return values;
     }
 
 
